68fe647d50173ea3a304b0b29668afe3a3bb40ac
Chris Dyson <c.dyson@mighway.co>
2017-12-07 15:19:12 +1300

    Ad 'no-branch' option to start command

--- src/commands/start.js
+++ src/commands/start.js
@@ -4,7 +4,8 @@ import { JIRA } from '../constants'
 export const command = 'start <issueIdOrKey>'
 export const description = 'Assigns a ticket to you, moves it To-Do creates a new branch for that ticket based on master of the current repo.'
 export const options = [
-  ['-f, --force', 'Force assignment of ticket to you, even if it is already assigned to someone else, or not in the current sprint.']
+  ['-f, --force', 'Force assignment of ticket to you, even if it is already assigned to someone else, or not in the current sprint.'],
+  ['-nb --no-branch', 'Assign the ticket to you without creating a branch']
 ]
 
 export async function action (args) {
